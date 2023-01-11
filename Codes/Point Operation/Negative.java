4ab5a80da2512e2e2e6501a0897a90c025465f97
Agung Rashif Madani <121701309+agung-madani@users.noreply.github.com>
2023-01-11 14:32:22 +0700

    Update Negative.java

--- Codes/Point Operation/Negative.java
+++ Codes/Point Operation/Negative.java
@@ -15,39 +15,35 @@ import javax.imageio.ImageIO;
  */
 public class Negative {
 
-    public static void main(String[] args) {
-        try {
-            // Read the input image file and store it in a BufferedImage object
-            BufferedImage img = ImageIO.read(new File("Z:\\citraJava\\lenaColor.jpg"));
+    public static void main(String[] args) throws IOException {
+        // Read the input image file and store it in a BufferedImage object
+        BufferedImage img = ImageIO.read(new File("Z:\\citraJava\\lenaColor.jpg"));
 
-            // get image's width and height
-            int width = img.getWidth();
-            int height = img.getHeight();
+        // get image's width and height
+        int width = img.getWidth();
+        int height = img.getHeight();
 
-            // apply negative filter to each pixel in the image
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    // get red, green, and blue values for current pixel
-                    int r = (img.getRGB(x, y) >> 16) & 0xff;
-                    int g = (img.getRGB(x, y) >> 8) & 0xff;
-                    int b = img.getRGB(x, y) & 0xff;
+        // apply negative filter to each pixel in the image
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // get red, green, and blue values for current pixel
+                int r = (img.getRGB(x, y) >> 16) & 0xff;
+                int g = (img.getRGB(x, y) >> 8) & 0xff;
+                int b = img.getRGB(x, y) & 0xff;
 
-                    // calculate negative values for red, green, and blue
-                    int rNew = 255 - r;
-                    int gNew = 255 - g;
-                    int bNew = 255 - b;
+                // calculate negative values for red, green, and blue
+                int rNew = 255 - r;
+                int gNew = 255 - g;
+                int bNew = 255 - b;
 
-                    // create new pixel with negative red, green, and blue values
-                    int pixelNew = (rNew << 16) | (gNew << 8) | bNew;
+                // create new pixel with negative red, green, and blue values
+                int pixelNew = (rNew << 16) | (gNew << 8) | bNew;
 
-                    // set pixel at (x, y) to new pixel value
-                    img.setRGB(x, y, pixelNew);
-                }
+                // set pixel at (x, y) to new pixel value
+                img.setRGB(x, y, pixelNew);
             }
-            // save modified image to file
-            ImageIO.write(img, "jpg", new File("Z:\\citraJava\\NegativeTest.jpg"));
-        } catch (IOException e) {
-            // Handle exception if image cannot be read from file
         }
+        // save modified image to file
+        ImageIO.write(img, "jpg", new File("Z:\\citraJava\\NegativeTest.jpg"));
     }
 }

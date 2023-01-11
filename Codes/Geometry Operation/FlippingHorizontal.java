97020d058aab1abfcc59f8f125ad90d8a4ba1039
Agung Rashif Madani <121701309+agung-madani@users.noreply.github.com>
2023-01-11 14:15:26 +0700

    Update FlippingHorizontal.java

--- Codes/Geometry Operation/FlippingHorizontal.java
+++ Codes/Geometry Operation/FlippingHorizontal.java
@@ -15,37 +15,34 @@ import javax.imageio.ImageIO;
  * @author agung
  */
 public class FlippingHorizontal {
-    public static void main(String[] args) {
-        try {
-            // Read the input image file and store it in a BufferedImage object
-            BufferedImage img1 = ImageIO.read(new File("Z:\\citraJava\\lena.jpg"));
 
-            // Get the width and height of the image
-            int width = img1.getWidth();
-            int height = img1.getHeight();
+    public static void main(String[] args) throws IOException {
+        // Read the input image file and store it in a BufferedImage object
+        BufferedImage img1 = ImageIO.read(new File("Z:\\citraJava\\lena.jpg"));
 
-            // Create a new BufferedImage object with the same width and height as the input image
-            BufferedImage img2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        // Get the width and height of the image
+        int width = img1.getWidth();
+        int height = img1.getHeight();
 
-            // Iterate over all the pixels in the image
-            for (int i = 0; i <= height - 1; i++) {
-                // Initialize k as the width of the image minus 1
-                int k = width - 1;
-                for (int j = 0; j <= width - 1; j++) {
-                    // Get the color of the pixel at (j, i) in the input image
-                    Color c = new Color(img1.getRGB(j, i));
-                    
-                    // Set the pixel at (k, i) in the output image to the same color
-                    img2.setRGB(k, i, c.getRGB());
-                    
-                    // Decrement k by 1
-                    k -= 1;
-                }
+        // Create a new BufferedImage object with the same width and height as the input image
+        BufferedImage img2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        // Iterate over all the pixels in the image
+        for (int i = 0; i <= height - 1; i++) {
+            // Initialize k as the width of the image minus 1
+            int k = width - 1;
+            for (int j = 0; j <= width - 1; j++) {
+                // Get the color of the pixel at (j, i) in the input image
+                Color c = new Color(img1.getRGB(j, i));
+
+                // Set the pixel at (k, i) in the output image to the same color
+                img2.setRGB(k, i, c.getRGB());
+
+                // Decrement k by 1
+                k -= 1;
             }
-            // save modified image to file
-            ImageIO.write(img2, "jpg", new File("Z:\\citraJava\\FlippingHorizontalTest.jpg"));
-        } catch (IOException e) {
-            // Handle exception if image cannot be read from file
         }
+        // save modified image to file
+        ImageIO.write(img2, "jpg", new File("Z:\\citraJava\\FlippingHorizontalTest.jpg"));
     }
 }

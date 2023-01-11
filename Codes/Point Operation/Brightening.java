1f6f6df9887dda334aea959a69935e0ddb6b951a
Agung Rashif Madani <121701309+agung-madani@users.noreply.github.com>
2023-01-11 14:30:01 +0700

    Update Brightening.java

--- Codes/Point Operation/Brightening.java
+++ Codes/Point Operation/Brightening.java
@@ -14,45 +14,42 @@ import javax.imageio.ImageIO;
  * @author agung
  */
 public class Brightening {
-    public static void main(String[] args) {
-        try {
-            // Read image from file and store in img
-            BufferedImage img = ImageIO.read(new File("Z:\\citraJava\\zelda.jpg"));
-            
-            // Get image's width and height
-            int width = img.getWidth();
-            int height = img.getHeight();
 
-            // Brightness adjustment value
-            int bright = 100; 
+    public static void main(String[] args) throws IOException {
+        // Read image from file and store in img
+        BufferedImage img = ImageIO.read(new File("Z:\\citraJava\\zelda.jpg"));
 
-            // Loop through each pixel in the image
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    // Get the original values of the pixel
-                    int r = (img.getRGB(x, y) >> 16) & 0xff;
-                    int g = (img.getRGB(x, y) >> 8) & 0xff;
-                    int b = img.getRGB(x, y) & 0xff;
+        // Get image's width and height
+        int width = img.getWidth();
+        int height = img.getHeight();
 
-                    // Adjust brightness by adding bright to each component
-                    int rNew = r + bright;
-                    int gNew = g + bright;
-                    int bNew = b + bright;
+        // Brightness adjustment value
+        int bright = 100;
 
-                    // Ensure that the RGB values are within the valid range (0-255)
-                    rNew = Math.max(0, Math.min(rNew, 255));
-                    gNew = Math.max(0, Math.min(gNew, 255));
-                    bNew = Math.max(0, Math.min(bNew, 255));
-                    
-                    // Set new RGB value for the pixel
-                    int pixelNew = (rNew << 16) | (gNew << 8) | bNew;
-                    img.setRGB(x, y, pixelNew);
-                }
+        // Loop through each pixel in the image
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // Get the original values of the pixel
+                int r = (img.getRGB(x, y) >> 16) & 0xff;
+                int g = (img.getRGB(x, y) >> 8) & 0xff;
+                int b = img.getRGB(x, y) & 0xff;
+
+                // Adjust brightness by adding bright to each component
+                int rNew = r + bright;
+                int gNew = g + bright;
+                int bNew = b + bright;
+
+                // Ensure that the RGB values are within the valid range (0-255)
+                rNew = Math.max(0, Math.min(rNew, 255));
+                gNew = Math.max(0, Math.min(gNew, 255));
+                bNew = Math.max(0, Math.min(bNew, 255));
+
+                // Set new RGB value for the pixel
+                int pixelNew = (rNew << 16) | (gNew << 8) | bNew;
+                img.setRGB(x, y, pixelNew);
             }
-            // save modified image to file
-            ImageIO.write(img, "jpg", new File("Z:\\citraJava\\brighteningTest.jpg"));
-        } catch (IOException e) {
-            // Handle exception if image cannot be read from file
         }
+        // save modified image to file
+        ImageIO.write(img, "jpg", new File("Z:\\citraJava\\brighteningTest.jpg"));
     }
 }

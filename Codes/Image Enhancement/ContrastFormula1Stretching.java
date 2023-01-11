a4963b3dabc02fc6e82775e934e5ee93a417937b
Agung Rashif Madani <121701309+agung-madani@users.noreply.github.com>
2023-01-11 14:27:22 +0700

    Update ContrastFormula1Stretching.java

--- Codes/Image Enhancement/ContrastFormula1Stretching.java
+++ Codes/Image Enhancement/ContrastFormula1Stretching.java
@@ -15,44 +15,40 @@ import javax.imageio.ImageIO;
  */
 public class ContrastFormula1Stretching {
 
-    public static void main(String[] args) {
-        try {
-            // Read the image file
-            BufferedImage img = ImageIO.read(new File("Z:\\citraJava\\zelda.jpg"));
+    public static void main(String[] args) throws IOException {
+        // Read the image file
+        BufferedImage img = ImageIO.read(new File("Z:\\citraJava\\zelda.jpg"));
 
-            // Get image's width and height
-            int width = img.getWidth();
-            int height = img.getHeight();
+        // Get image's width and height
+        int width = img.getWidth();
+        int height = img.getHeight();
 
-            // Contrast Stretching adjustment values
-            int c1 = 50; // start
-            int c2 = 150; // end
+        // Contrast Stretching adjustment values
+        int c1 = 50; // start
+        int c2 = 150; // end
 
-            // Perform contrast stretching on the image
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-                    // Get the original values of the pixel
-                    int r = (img.getRGB(x, y) >> 16) & 0xFF;
-                    int g = (img.getRGB(x, y) >> 8) & 0xFF;
-                    int b = img.getRGB(x, y) & 0xFF;
+        // Perform contrast stretching on the image
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // Get the original values of the pixel
+                int r = (img.getRGB(x, y) >> 16) & 0xFF;
+                int g = (img.getRGB(x, y) >> 8) & 0xFF;
+                int b = img.getRGB(x, y) & 0xFF;
 
-                    // Calculate the new values for the red, green, and blue components
-                    int rNew = transformColor(r, c1, c2);
-                    int gNew = transformColor(g, c1, c2);
-                    int bNew = transformColor(b, c1, c2);
+                // Calculate the new values for the red, green, and blue components
+                int rNew = transformColor(r, c1, c2);
+                int gNew = transformColor(g, c1, c2);
+                int bNew = transformColor(b, c1, c2);
 
-                    // Create a new pixel value using the new component values
-                    int pixelNew = (rNew << 16) | (gNew << 8) | bNew;
+                // Create a new pixel value using the new component values
+                int pixelNew = (rNew << 16) | (gNew << 8) | bNew;
 
-                    // Set the pixel value in the image
-                    img.setRGB(x, y, pixelNew);
-                }
+                // Set the pixel value in the image
+                img.setRGB(x, y, pixelNew);
             }
-            // save modified image to file
-            ImageIO.write(img, "jpg", new File("Z:\\citraJava\\ContrastFormula1StretchingTest.jpg"));
-        } catch (IOException e) {
-            // Handle exception if image cannot be read from file
         }
+        // save modified image to file
+        ImageIO.write(img, "jpg", new File("Z:\\citraJava\\ContrastFormula1StretchingTest.jpg"));
     }
 
     public static int transformColor(int color, int c1, int c2) {

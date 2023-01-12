88a4350130eb37761989e89937757e57f8b8fc4d
Agung Rashif Madani <121701309+agung-madani@users.noreply.github.com>
2023-01-12 13:28:07 +0700

    Update FlippingVertical.java

--- Codes/Geometry Operation/FlippingVertical.java
+++ Codes/Geometry Operation/FlippingVertical.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.mavenproject1;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -19,36 +20,18 @@ public class FlippingVertical {
         // Read the input image file and store it in a BufferedImage object
         BufferedImage img1 = ImageIO.read(new File("Z:\\citraJava\\lena.jpg"));
 
-        // Get the width and height of the image
         int width = img1.getWidth();
         int height = img1.getHeight();
 
-        // Create a new BufferedImage object with the same width and height as the input image
         BufferedImage img2 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        // Iterate over all the pixels in the image
         for (int x = 0; x < width; x++) {
-            // Initialize k as the height of the image minus 1
-            int k = height - 1;
             for (int y = 0; y < height; y++) {
-                // Get the color of the pixel at (i, j) in the input image
-                int r = (img1.getRGB(x, y) >> 16) & 0xFF;
-                int g = (img1.getRGB(x, y) >> 8) & 0xFF;
-                int b = img1.getRGB(x, y) & 0xFF;
-
-                // Create a new pixel value with the extracted color values
-                int pixelNew = (r << 16) | (g << 8) | b;
-
-                // Set the pixel in the new image at the coordinates (x, k) with the new pixel value
-                // So for every x,y in original it is saved in x,k in new image 
-                // k is calculated as height -1 to reverse the y axis.
-                img2.setRGB(x, k, pixelNew);
-
-                // Decrement k by 1
-                k -= 1;
+                Color c = new Color(img1.getRGB(x, y));
+                img2.setRGB(x, height-1-y, c.getRGB());
             }
         }
-        // Save the modified image to a file
+        // save modified image to file
         ImageIO.write(img2, "jpg", new File("Z:\\citraJava\\FlippingVerticalTest2.jpg"));
     }
 }
